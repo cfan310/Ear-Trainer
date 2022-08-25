f041ede5f579ce70d045db30505e0d7801aae398
cfan310 <cfanning43@gmail.com>
2022-08-24 17:18:56 -0700

    adding hidden button

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ console.log(keys);
 
 const button1 = document.getElementById("buttonTest");
 
+var textAfterClick = document.getElementById("textAfterClick");
+
+var hiddenText = textAfterClick.innerHTML;
+
+hiddenText = "SURPRISE";
+
 // click function to dispay innerhtml if button is clicked
+
+function displayText() {
+  textAfterClick.innerHTML = "SURPRISE";
+  button1.style.color = "orange";
+}
