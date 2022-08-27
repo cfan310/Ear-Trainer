0604a8265b4fca19ed63a8398bfcff6b0ab2aa49
cfan310 <cfanning43@gmail.com>
2022-08-26 20:01:12 -0700

    problems to solve

---
+++ piano.js
@@ -0,0 +1,13 @@
+// store keys
+// distinguish between sharps and flats
+// solve the EF and BC problem
+
+// create general keyboard in css
+
+// apply functionality to the keyboard upon click (begin with pop us)
+
+// experiment with importing audio files upon click for pitches
+
+// create physical guitar note location logic
+
+// eventually choose three notes to play in succession
