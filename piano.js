d517d644d23e878181c905362925a3e373969074
cfan310 <cfanning43@gmail.com>
2022-08-26 20:08:58 -0700

    button version before deletion

--- piano.js
+++ piano.js
@@ -1,13 +1,8 @@
 // store keys
 // distinguish between sharps and flats
 // solve the EF and BC problem
-
 // create general keyboard in css
-
 // apply functionality to the keyboard upon click (begin with pop us)
-
 // experiment with importing audio files upon click for pitches
-
 // create physical guitar note location logic
-
 // eventually choose three notes to play in succession
